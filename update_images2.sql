8acfbc0410e67125eef7d02800ed5ba654674c20
jiangxingzhao <jiangxingzhao@hotmail.com>
2026-06-13 22:08:28 +0800

    为SP011-SP025共15个商品绘制SVG图片+数据库更新脚本

---
+++ update_images2.sql
@@ -0,0 +1,16 @@
+-- 补充商品图片路径
+UPDATE products SET imagepath = '~/Content/images/products/prod_ambrosial.svg'     WHERE code = 'SP011';
+UPDATE products SET imagepath = '~/Content/images/products/prod_deluxe_milk.svg'    WHERE code = 'SP012';
+UPDATE products SET imagepath = '~/Content/images/products/prod_tsingtao.svg'       WHERE code = 'SP013';
+UPDATE products SET imagepath = '~/Content/images/products/prod_budweiser.svg'      WHERE code = 'SP014';
+UPDATE products SET imagepath = '~/Content/images/products/prod_nongfu.svg'         WHERE code = 'SP015';
+UPDATE products SET imagepath = '~/Content/images/products/prod_cestbon.svg'        WHERE code = 'SP016';
+UPDATE products SET imagepath = '~/Content/images/products/prod_dumpling.svg'       WHERE code = 'SP017';
+UPDATE products SET imagepath = '~/Content/images/products/prod_tangyuan.svg'       WHERE code = 'SP018';
+UPDATE products SET imagepath = '~/Content/images/products/prod_headshoulders.svg'  WHERE code = 'SP019';
+UPDATE products SET imagepath = '~/Content/images/products/prod_safeguard.svg'      WHERE code = 'SP020';
+UPDATE products SET imagepath = '~/Content/images/products/prod_peanut_oil.svg'     WHERE code = 'SP021';
+UPDATE products SET imagepath = '~/Content/images/products/prod_flm_rice.svg'       WHERE code = 'SP022';
+UPDATE products SET imagepath = '~/Content/images/products/prod_dove_chocolate.svg' WHERE code = 'SP023';
+UPDATE products SET imagepath = '~/Content/images/products/prod_wantwant.svg'       WHERE code = 'SP024';
+UPDATE products SET imagepath = '~/Content/images/products/prod_kangshifu.svg'      WHERE code = 'SP025';
